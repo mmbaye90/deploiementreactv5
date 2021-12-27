51fa726e1df0412d7d225442802566994280e0bd
Mbaye <mmbaye1@hotmail.fr>
2021-12-27 18:12:10 +0100

    Updates

---
+++ precache-manifest.f477553d1de72eefd9447e98e19c712c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c46272d09eb60eeac2fe",
+    "url": "./static/css/main.b20fe129.chunk.css"
+  },
+  {
+    "revision": "c46272d09eb60eeac2fe",
+    "url": "./static/js/main.f0ec7296.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ace4b0af2b7460efbc97",
+    "url": "./static/js/2.ccc0db1b.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "./static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "393e20b74485e1ae1f2a970b757b4d85",
+    "url": "./index.html"
+  }
+];
